3c969c7e4e317900c06c872a93aae8a05cc9c2df
안다솜 <ektha5032@gmail.com>
2025-11-20 11:04:09 +0900

    feat: add subdomain change modal to project detail page

--- platform-frontend/src/pages/ProjectDetail.jsx
+++ platform-frontend/src/pages/ProjectDetail.jsx
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ProjectDetail.css";
 
 const ProjectDetail = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [newSubdomain, setNewSubdomain] = useState("");
+
+  // 모달 핸들링
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+    setNewSubdomain("");
+  };
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
     <section className="project-detail-section">
       <div className="wrap">
@@ -19,7 +32,9 @@ const ProjectDetail = () => {
             >
               subdomain.qwik.com
             </a>
-            <button className="change-subdomain-btn">변경하기</button>
+            <button className="change-subdomain-btn" onClick={handleOpenModal}>
+              변경하기
+            </button>
           </div>
         </div>
         <div className="resource-container">
@@ -61,6 +76,38 @@ const ProjectDetail = () => {
           </div>
         </div>
       </div>
+      {isModalOpen && (
+        <div className="modal-bg">
+          <div className="modal-popup change-subdomain-modal">
+            <div className="text-box">
+              <p className="title">변경 전 확인해주세요!</p>
+              <div className="notice-box">
+                <p>변경 버튼을 눌러야만 변경됩니다.</p>
+                <p>
+                  변경시, 기존 주소는 사용 불가하며<br></br> 변경 처리로 인해
+                  일시적으로 접속이 불가할 수 있습니다.{" "}
+                  <span>(약 2분 소요)</span>
+                </p>
+              </div>
+            </div>
+
+            <div className="input-box">
+              <input
+                type="text"
+                className="input-subdomin"
+                placeholder="원하는 서브도메인을 입력해주세요."
+              />
+              <button className="check for duplicates">중복확인</button>
+            </div>
+            <div className="confirm-btn-box btn-box">
+              <button className="cancel-btn" onClick={handleCloseModal}>
+                닫기
+              </button>
+              <button className="change-btn impact">변경</button>
+            </div>
+          </div>
+        </div>
+      )}
     </section>
   );
 };
